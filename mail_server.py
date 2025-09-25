bce466a06d877ff6fe768e28d6a5a2777decb4bc
mayykhan <maykhan@usc.edu>
2025-09-25 05:40:29 +0000

    finished server

--- mail_server.py
+++ mail_server.py
@@ -131,7 +131,15 @@ def delete_mail_route(mail_id: str):
         bool: True if the mail was deleted, False otherwise
     """
     # TODO: implement this function
-    pass # remove this line
+    if delete_mail(mail_id):
+        res = jsonify({'success': True})
+        res.status_code = 200 
+        return res
+    else:
+        res = jsonify({'success': False})
+        res.status_code = 404
+        return res
+
 
 @app.route('/mail/<mail_id>', methods=['GET'])
 def get_mail_route(mail_id: str):
@@ -163,9 +171,25 @@ def get_inbox_route(recipient: str):
     res.status_code = 200
     return res
 
-# TODO: implement a rout e to get all mail entries for a sender
+# TODO: implement a route to get all mail entries for a sender
 # HINT: start with soemthing like this:
 #   @app.route('/mail/sent/<sender>', ...)
+@app.route('/mail/sent/<sender>', methods=['GET'])
+def get_sent_route(sender: str):
+    """
+    Summary: Gets all mail entries for a sender from the json file
+
+    Args:
+        sender (str): The sender of the mail
+
+    Returns:
+        list: A list of dictionaries representing the mail entries
+    """
+
+    res = jsonify(get_sent(sender))
+    res.status_code = 200
+    return res
+
 
 
 if __name__ == '__main__':
